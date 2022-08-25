ec6723b0362125e0170a76d11695e387b02f4d4a
rensilva5 <renzoasilva@gmail.com>
2022-08-24 20:36:45 -0400

    new aws and deploy

---
+++ index.js
@@ -0,0 +1,16 @@
+import { express } from "express";
+import { cors } from "cors";
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+ 
+const PORT = 4000
+
+
+app.get('/', (req, res) => res.send ('my api 🚦'))
+
+app.listen(PORT, ()=> {
+    console.log(`listening on port ${PORT}`)
+})
